3547e9dea678fc32e0ec4c9e1d10c330c5ca04d0
Aum Panchal <aumpanchal2004@gmail.com>
2025-09-04 02:54:36 +0530

    Updated Transport

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const supabase = createClient(
 );
 
 // Email transporter (using Gmail as example)
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
